772cdbcb7675bd27efb8f1c3efaaab722dc12ebc
arunkbadhai24 <arunkb2024@gmail.com>
2024-09-22 17:59:29 +0000

    first commit

---
+++ Loop.py
@@ -0,0 +1,29 @@
+
+print("*********Example1 ****************")
+for i in range(5):
+    print(i)
+
+print("*********Example2 ****************")   
+
+for i in range(1,10,1):
+    print(i)
+
+print("*********Example3 ****************")
+# Using step size
+for i in range(1,10,2):
+    print(i)
+
+print("*********Example4 ****************")
+#Reverse using step size
+for i in range(10,1,-1):
+    print(i)
+
+
+print("*********Example5 ****************")
+## String
+
+str = "Arun Kumar"
+
+for i in str:
+    print(i)
+    
